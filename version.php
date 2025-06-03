7e2bc454e31c27d04807937de557981317d2ac0c
Phạm Cương <vkpan297@gmail.com>
2025-06-03 16:40:47 +0700

    fix

--- version.php
+++ version.php
@@ -2,5 +2,5 @@
 
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_custom_service';
-$plugin->version  = 2022071733;
+$plugin->version  = 2022071736;
 $plugin->requires = 2016052314;
